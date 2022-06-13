345bf72b884bac47d76bf90fcd99b7efcf75f49e
Franzley <fbonhomme9721@gmail.com>
2022-06-13 06:17:02 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomize();
 };
+
+function randomize() {
+  var arrNum = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  var suitArr = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
+  var randomizeIndex = Math.floor(Math.random() * arrNum.length);
+  var randomizeSuit = Math.floor(Math.random() * suitArr.length);
+
+  var suits = document.getElementsByClassName("icon");
+  var numb = document.getElementsByClassName("number");
+
+  suits[0].innerHTML = suitArr[randomizeSuit];
+  suits[1].innerHTML = suitArr[randomizeSuit];
+  numb[0].innerHTML = arrNum[randomizeIndex];
+}
